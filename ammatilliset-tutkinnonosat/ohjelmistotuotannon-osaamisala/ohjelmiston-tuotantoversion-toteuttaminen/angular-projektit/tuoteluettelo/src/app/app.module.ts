2aa60b1ad204f5cf5e33fa7db21a13aa78c7ffa9
Noa Julkunen <s1900877@edu.bc.fi>
2021-05-11 15:48:00 +0300

    muokattu sään esitystapaa > ylin ja alin lämpötila, . ==, , pyöristys

--- ammatilliset-tutkinnonosat/ohjelmistotuotannon-osaamisala/ohjelmiston-tuotantoversion-toteuttaminen/angular-projektit/tuoteluettelo/src/app/app.module.ts
+++ ammatilliset-tutkinnonosat/ohjelmistotuotannon-osaamisala/ohjelmiston-tuotantoversion-toteuttaminen/angular-projektit/tuoteluettelo/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { PuhelinComponent } from './puhelin/puhelin.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PuhelinComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FontAwesomeModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
